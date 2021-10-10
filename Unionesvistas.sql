7fe8a997e76812df011d02b4a7bfec533b005d18
xDavePachx <92230662+xDavePachx@users.noreply.github.com>
2021-10-10 12:27:25 -0600

    se modifico el scritp, modificando la vista anterior ListadoProductos

--- Unionesvistas.sql
+++ Unionesvistas.sql
@@ -28,3 +28,22 @@ ON CustomerID = o.CustomerID;
 
 
 SELECT Producto, Categoria, Proveedores FROM ListadoProductos;
+
+
+
+--Modificacion de vista 
+-- de tal forma que muestre los nombres que estan incluidos o no en las ordenes
+ALTER VIEW ListadoProductos 
+
+AS
+
+SELECT
+	p.ProductName Producto, c.CategoryName Categoria, o.OrderID NumeroDeOrden
+FROM 
+	Products p
+RIGHT JOIN [Order Details] o
+ON  p.ProductID = o.ProductID
+INNER JOIN Categories c
+ON p.CategoryID = c.CategoryID;
+
+SELECT Producto, Categoria, NumeroDeOrden FROM ListadoProductos
